31149c809f5381687e89bf5d59983ced532c5593
SarvY1999 <sarveshy193@gmail.com>
2023-06-13 22:00:06 +0530

    adds register functionality

--- controllers/user.js
+++ controllers/user.js
@@ -1,5 +1,17 @@
-const register = (req, res) => {
-    res.send('Register User');
+const { StatusCodes } = require('http-status-codes');
+const User = require('../models/user');
+const userError = require('../error/userError');
+
+const register = async (req, res) => {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+        throw new userError();
+    };
+
+    const user = await User.create({ ...req.body });
+
+    res.status(StatusCodes.CREATED).json(user);
 };
 
 const login = (req, res) => {
